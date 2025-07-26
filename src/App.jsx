cb16d4afe6bf28a8ced2ebe546469ca58ee19e73
tharidhu1225 <jayasooriyatharidhu@gmail.com>
2025-07-26 23:27:57 +0530

    headder update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/header";
+import Header from './components/Header';
 import HomePage from "./pages/homePage";
 import ResourcesPage from "./pages/resourcesPage";
 import Footer from "./components/Footer";
